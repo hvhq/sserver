7aaa3882f458bb20509e56502872d1fa27abc4e1
Hien Van Huynh <vanhienhuynh396@gmail.com>
2021-02-15 14:58:25 +0700

    runable log t

--- files/tscript.js
+++ files/tscript.js
@@ -1,15 +1,15 @@
 var url = "http://localhost:4321";
 var snakeDir = [0.0, 0.0];	//vector with root is O(0,0) ~ is: the mouse place-the head place
-var snakeBody = [{x:100.0, y:100.0}]; //address on the global map
+var snakeBody = [{x:98.0, y:100.0}]; //address on the global map
 var bodyDistance = 10.0; //real ~ on global
 var stepMove = 3; //on real
 var adjustRatio = {x:0, y:0}
-var backgroundPos = [0, 0]; //percent
+var backgroundPos = [0, 0]; 
 var decideToMove = null;
 var screenWidth = null;
 var screenHeight = null;
 
-function increasex(){
+/*function increasex(){
 	var x = document.getElementById("xholder");
 	var y = parseInt(x.textContent) + 1;
 	x.innerHTML = y;
@@ -22,13 +22,11 @@ function decreasex(){
 	x.innerHTML = y;
 	
 	console.log("decreased");
-}
+}*/
 function goplayboard(){
 	//send post request
-	//send name
-	//get place of other
-	//get place of this
-	//get place of food
+	//send name, where is the head and body
+	//get place of other snake, food, 
 	
 	var pname = document.getElementById("pname").value;
 	
@@ -38,15 +36,27 @@ function goplayboard(){
 	request.send(JSON.stringify({
 		name:pname
 	}));
+	snakeBody[0].x = 0;
+	snakeBody[0].y = 0;
 	request.onload = function() {
 		console.log("sent your info");
 		var jsonResponse = JSON.parse(this.responseText);
 		var container = document.getElementById("container");
 		container.innerHTML = jsonResponse['pl'];
 		container.style.backgroundImage = "url('bgpl.png')";
-		initialize();
-		addTail(true);
-		addTail(true);
+		initialize(jsonResponse);
+		
+		/*if (jsonResponse != null) {
+			snakeBody[0].x = jsonResponse.u[0].x;
+			snakeBody[0].y = jsonResponse.u[0].y;
+			for (var it=1; it < jsonResponse.u.length; it++) {
+				addTail(true, true, jsonResponse.u[it]);
+			}
+		}*/
+		startMove();
+		//startMove();
+		//addTail(true);
+		//addTail(true);
 		/*var newelement = document.createElement('div');
 		newelement.innerHTML = this.responseText;
 		x = document.getElementById("toreplace");
@@ -64,10 +74,14 @@ function sendPostRequest(url, stringdata, onLoadFunction){
 	request.onload = onLoadFunction();
 }
 
-function initialize() {
+function initialize(jsonRes = null) {
 	screenWidth = document.getElementById("container").clientWidth;
 	screenHeight = document.getElementById("container").clientHeight;	
 	adjustRatio = {x:100/screenWidth, y:100/screenHeight}
+	if(jsonRes !== null){
+		snakeBody[0].x = 25;
+		snakeBody[0].y = 25;
+	}
 	//snakeBody[0]={x:screenWidth / 2.0, y: screenHeight / 2.0};
 }
 
@@ -75,8 +89,8 @@ function startMove() {
 	if (decideToMove != null){
 		return;
 	}
-	initialize();
 	decideToMove = setInterval(moveTheSnake, 20);
+		document.getElementById("positionzone4").innerHTML = snakeBody[0].x + ":"+snakeBody[0].y;
 }
 function stopMove() {
 	if (decideToMove==null){
@@ -87,46 +101,41 @@ function stopMove() {
 	decideToMove = null;
 }
 
-function moveTheSnake() {
+function moveTheSnake() {	
+	document.getElementById("positionzone2").innerHTML = snakeBody[0].x + ":"+snakeBody[0].y;	
 	lengthVector = Math.sqrt(snakeDir[0]*snakeDir[0] + snakeDir[1]*snakeDir[1]);
 	var add ={};
-	add.x = stepMove /lengthVector * snakeDir[0];
-	add.y = stepMove /lengthVector * snakeDir[1];
-	
+	if (lengthVector > 0){
+		add.x = stepMove /lengthVector * snakeDir[0];
+		add.y = stepMove /lengthVector * snakeDir[1];
+	}else{
+		add.x = 0;
+		add.y = 0;
+	}
+	//move real head	
 	snakeBody[0].x += add.x;
 	snakeBody[0].y += add.y;
-
-	//backgroundPos[0] -= ((add.x / screenWidth) * 100);
-	//backgroundPos[1] -= ((add.y / screenHeight) * 100);
 
 	backgroundPos[0] -= add.x;
 	backgroundPos[1] -= add.y;
 	//move eye
 	moveEyes();
-	document.getElementById("positionzone2").innerHTML=add.x+":"+add.y;
-
 
 	//move head~mean move the whole body and head stay 
 	//move body along with the screen
+	document.getElementById("positionzone3").innerHTML = snakeBody[0].x + ":"+snakeBody[0].y;
+
 	for (var it=1; it<snakeBody.length; it++){
-		moveSnakeBodyPartImage(it, {x:(snakeBody[0].x-add.x) * adjustRatio.x, y:(snakeBody[0].y-add.y) * adjustRatio.y});
+		moveSnakeBodyPartImage(it, {x:(snakeBody[0].x-add.x) * adjustRatio.x, 
+									y:(snakeBody[0].y-add.y) * adjustRatio.y});
 	}
 	//adjust the body 
 	for (var it = 1; it<snakeBody.length; it++){
 		moveBodyPartBackend(it);
 
-		var prevToWard = {x:(snakeBody[it].x-snakeBody[it-1].x)*adjustRatio.x,y:(snakeBody[it].y-snakeBody[it-1].y)*adjustRatio.y};
-		dis1 = Math.sqrt(prevToWard.x*prevToWard.x+prevToWard.y*prevToWard.y);
-		document.getElementById("positionzone4").innerHTML = dis1;
+		var prevToWard = {x:(snakeBody[it].x-snakeBody[it-1].x)*adjustRatio.x,
+							y:(snakeBody[it].y-snakeBody[it-1].y)*adjustRatio.y};
 		moveSnakeBodyPartImage(it, prevToWard);
-		if(it == 1){
-			var thisElem = document.getElementById("body"+it);
-			var prevElem = document.getElementById("body"+(it-1));
-			var difx = parseFloat(thisElem.style.top)-parseFloat(prevElem.style.top);
-			var dify = parseFloat(thisElem.style.left)-parseFloat(prevElem.style.left);
-			var dis  = Math.sqrt(difx*difx+dify*dify);
-			document.getElementById("positionzone3").innerHTML = dis;
-		}
 	}
 	
 	moveTheShapeOfSnake();
@@ -160,32 +169,29 @@ function moveHeadBackend() {
 
 }
 function moveBodyPartBackend(it) {
-	//disToPrev = Math.sqrt(()*()+()*());
 	var disToPrev = Math.sqrt((snakeBody[it].x-snakeBody[it-1].x)*(snakeBody[it].x-snakeBody[it-1].x)+(snakeBody[it].y-snakeBody[it-1].y)*(snakeBody[it].y-snakeBody[it-1].y));
-	//var add = {};
-	//add.x = snakeBody[it-1].x - bodyDistance/disToPrev * (snakeBody[it-1].x-snakeBody[it].x) - snakeBody[it].x;
-	//add.y = snakeBody[it-1].y - bodyDistance/disToPrev * (snakeBody[it-1].y-snakeBody[it].y) - snakeBody[it].y;
-	//snakeBody[it].x += add.x;
-	//snakeBody[it].y += add.y;
+
 	snakeBody[it].x = snakeBody[it-1].x - bodyDistance/disToPrev * (snakeBody[it-1].x-snakeBody[it].x);
 	snakeBody[it].y = snakeBody[it-1].y - bodyDistance/disToPrev * (snakeBody[it-1].y-snakeBody[it].y);
-	
-	disToPrev = Math.sqrt((snakeBody[it].x-snakeBody[it-1].x)*(snakeBody[it].x-snakeBody[it-1].x)+(snakeBody[it].y-snakeBody[it-1].y)*(snakeBody[it].y-snakeBody[it-1].y));
-	document.getElementById("positionzone4").innerHTML=disToPrev.toString();
-	//return add;
 }
 
 //to increase the responsive speed
 function moveSnakeBodyPartImage(it, add) {
 	var elem = document.getElementById("body"+it);
 	var prevElem = document.getElementById("body"+(it-1));
+	//console.log(add.x);
+	//console.log(parseFloat(prevElem.style.left));
 	elem.style.left = (parseFloat(prevElem.style.left)+add.x)+"%";
 	elem.style.top = (parseFloat(prevElem.style.top)+add.y)+"%";
 }
 
-function addTail(setZIndex = false) {
-	var pointTail = getPointTail(snakeBody);
-	console.log(pointTail);
+function addTail(setZIndex = false, isUsePos=false, pos={x:0,y:0}) {
+	var pointTail = {};
+	if(isUsePos){
+		pointTail = pos;
+	} else{ 
+		pointTail = getPointTail(snakeBody);
+	}
 	snakeBody.push(pointTail);
 
 	var newTailImage = document.createElement('DIV');
@@ -193,8 +199,10 @@ function addTail(setZIndex = false) {
 	newTailImage.className = "circle";
 	
 	var prevIt = snakeBody.length - 2;
-	var diff = {x: (pointTail.x - snakeBody[prevIt].x), y:(pointTail.y-snakeBody[prevIt].y)};
-	console.log(diff.x, diff.y);
+	var diff = {x: (pointTail.x - snakeBody[prevIt].x) * adjustRatio.x,
+				 y:(pointTail.y-snakeBody[prevIt].y) * adjustRatio.y};
+	//console.log(diff.x, diff.y);
+	//console.log(Math.sqrt(diff.x*diff.x+diff.y* diff.y));
 	
 	var prevElem = document.getElementById("body"+prevIt);
 	newTailImage.style.top = (parseFloat(prevElem.style.top) + diff.y)+"%";
@@ -215,8 +223,8 @@ function getPointTail(snakeBody){
 	if (snakeLength <= 1){
 		dirLength = Math.sqrt(snakeDir[0]*snakeDir[0] + snakeDir[1]*snakeDir[1]);
 		if ( dirLength > 0){
-			tail.x = (bodyDistance / dirLength + 1 ) * snakeBody[0].x;// - bodyDistance / dirLength * snakeDir[0];
-			tail.y = (bodyDistance / dirLength + 1 ) * snakeBody[0].y;// - bodyDistance / dirLength * snakeDir[1];
+			tail.x = bodyDistance / dirLength * snakeDir[0] + snakeBody[0].x;// - bodyDistance / dirLength * snakeDir[0];
+			tail.y = bodyDistance / dirLength * snakeDir[1] + snakeBody[0].y;// - bodyDistance / dirLength * snakeDir[1];
 			//add.x = bodyDistance / dirLength * snakeBody[0].x;
 			//add.y = bodyDistance / dirLength * snakeBody[0].y;
 		} else {
